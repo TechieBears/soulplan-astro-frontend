3231ae06eef2453e67c1dc132ede894852699dd9
MohanMT <mohanmamidala2004@gmail.com>
2025-09-12 10:36:12 +0530

    contact changes

--- src/env.jsx
+++ src/env.jsx
@@ -1,18 +1,17 @@
 export const environment = {
     // -------------------- Local Server Api URL --------------------
-    // baseUrl: 'http://192.168.0.134:6020',
-    // production: false,
+    baseUrl: 'http://192.168.0.134:6020',
+    production: false,
 
 
-    imageBaseUrl: 'http://192.168.0.134:6020',
     // production: false,
     // -------------------- Ngrok Server Api URL --------------------
     // baseUrl: 'https://c938f75e4997.ngrok-free.app/api/',
     // production: false
 
     // -------------------- Devlopment Server Api URL --------------------
-    baseUrl: 'https://soulplan-astro-backend.vercel.app/api/',
-    production: true,
+    // baseUrl: 'https://soulplan-astro-backend.vercel.app/api/',
+    // production: true,
 
 
     // -------------------- Live Server Api URL --------------------
