546fabbbcc3ee3b2601a483c2a07fc58798367e3
MohanMT <mohanmamidala2004@gmail.com>
2025-10-14 15:52:50 +0530

    homepage,whatsp

--- src/routes/ProjectRoutes.jsx
+++ src/routes/ProjectRoutes.jsx
@@ -206,14 +206,6 @@ const ProjectRoutes = () => {
                         <Route path="*" element={<ErrorPage />} />
                     </Routes>
                     <HomeFooter />
-                    <a
-                        href={`https://wa.me/${8693000900}`}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        className=" fixed bottom-6 right-6 z-50 bg-green-500 hover:bg-green-600 text-white p-3.5 rounded-full shadow-lg transition-all duration-300"
-                    >
-                        <Whatsapp size={30} />
-                    </a>
                 </main>
             )}
 
