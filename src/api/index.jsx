39b227ad4986e25311a60d368fb97ebaf1f0fc3a
rydgo <jaiswalavashesh1@gmail.com>
2025-12-09 15:46:41 +0530

    banner image update changes

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -771,8 +771,23 @@ export const getAllBanners = async (data) => {
 
 export const addBanner = async (data) => {
     const url = `${environment.baseUrl}banners/create`;
+    const formData = new FormData();
+    for (const key in data) {
+        if (Array.isArray(data[key])) {
+            // Handle arrays - stringify arrays of objects, append arrays of primitives individually
+            if (data[key].length > 0 && typeof data[key][0] === 'object') {
+                formData.append(key, JSON.stringify(data[key]));
+            } else {
+                data[key].forEach((item) => {
+                    formData.append(key, item);
+                });
+            }
+        } else {
+            formData.append(key, data[key]);
+        }
+    }
     try {
-        const response = await axios.post(url, data)
+        const response = await axios.post(url, formData)
         return response.data
     }
     catch (err) {
@@ -783,8 +798,23 @@ export const addBanner = async (data) => {
 
 export const editBanner = async (id, data) => {
     const url = `${environment.baseUrl}banners/update?id=${id}`;
+    const formData = new FormData();
+    for (const key in data) {
+        if (Array.isArray(data[key])) {
+            // Handle arrays - stringify arrays of objects, append arrays of primitives individually
+            if (data[key].length > 0 && typeof data[key][0] === 'object') {
+                formData.append(key, JSON.stringify(data[key]));
+            } else {
+                data[key].forEach((item) => {
+                    formData.append(key, item);
+                });
+            }
+        } else {
+            formData.append(key, data[key]);
+        }
+    }
     try {
-        const response = await axios.put(url, data)
+        const response = await axios.put(url, formData)
         return response.data
     }
     catch (err) {
